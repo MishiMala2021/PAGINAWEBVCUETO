548a5d2229c9df17cca6e0bac7848b9356d9cc1b
MishiMala2021 <98244900+MishiMala2021@users.noreply.github.com>
2023-08-04 13:04:35 -0500

    Delete js directory

--- js/navegador.js
+++
@@ -1,6 +0,0 @@
-const lista = document.querySelector('.menu_links'); 
-const iconomenu = document.querySelector('.menu_icon'); 
-
-iconomenu.addEventListener('click', function() {
-    lista.classList.toggle('menu_links--show');
-});
